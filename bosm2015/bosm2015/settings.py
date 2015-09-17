e3e838007c055f799f72861040880bc4caac39d9
Satwik Bhattamishra <satwik55@gmail.com>
2015-09-17 14:19:23 +0530

    removed original database settings

--- bosm2015/bosm2015/settings.py
+++ bosm2015/bosm2015/settings.py
@@ -81,16 +81,6 @@ WSGI_APPLICATION = 'bosm2015.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'bosm_2015',                      # Or path to database file if using sqlite3.
-#         'USER': 'root',                      # Not used with sqlite3.
-#         'PASSWORD': 'foxfixedwagongun',                  # Not used with sqlite3.
-#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
 
 DATABASES = {
     'default': {
